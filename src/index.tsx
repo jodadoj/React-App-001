9c488f55e71a88b334bd2731816479081d28617b
Josiah Adojutelegan <JosiahAdojutelegan@gmail.com>
2022-11-18 21:07:54 +0000

    list comment

--- src/index.tsx
+++ src/index.tsx
@@ -2,3 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"));
+/*
+React.DOM.render(<ul><li>One</li><li>Two</li></ul>, document.getElementById("root"));
+*/
